935e02c8874b83b86ce26dc2ee1b8dcd9083ab88
Theisson <theissondias194@gmail.com>
2026-04-21 23:01:32 -0300

    GET client by id, GET clients

--- src/main/java/io/github/theisson/webservices/controllers/ClientController.java
+++ src/main/java/io/github/theisson/webservices/controllers/ClientController.java
@@ -1,10 +1,39 @@
 package io.github.theisson.webservices.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.theisson.webservices.dto.ClientResponseDTO;
+import io.github.theisson.webservices.services.application.GetClient;
+import io.github.theisson.webservices.services.application.ListClients;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
-    // TODO: Implementar os endpoints para criar, ler, atualizar e deletar clientes
+    
+    private final GetClient getClient;
+    private final ListClients listClients;
+
+    public ClientController(GetClient getClient, ListClients listClients) {
+        this.getClient = getClient;
+        this.listClients = listClients;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientResponseDTO>> findAll(Pageable pageable) {
+        Page<ClientResponseDTO> list = listClients.execute(pageable);
+
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientResponseDTO> findById(@PathVariable Long id) {
+        ClientResponseDTO client = getClient.execute(id);
+
+        return ResponseEntity.ok(client);
+    }
 }
--- src/main/java/io/github/theisson/webservices/services/application/ListClients.java
+++ src/main/java/io/github/theisson/webservices/services/application/ListClients.java
@@ -1,26 +1,26 @@
 package io.github.theisson.webservices.services.application;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import io.github.theisson.webservices.dto.ClientResponseDTO;
-import io.github.theisson.webservices.exceptions.ResourceNotFoundException;
 import io.github.theisson.webservices.models.entities.Client;
 import io.github.theisson.webservices.repositories.ClientRepository;
 
 @Service
-public class GetClient {
+public class ListClients {
     
     private final ClientRepository clientRepository;
 
-    public GetClient(ClientRepository clientRepository) {
+    public ListClients(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
 
     @Transactional(readOnly = true)
-    public ClientResponseDTO execute(Long id) {
-        Client entity = clientRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado com id: " + id));
+    public Page<ClientResponseDTO> execute(Pageable pageable) {
+        Page<Client> result = clientRepository.findAll(pageable);
 
-        return new ClientResponseDTO(entity);
+        return result.map(ClientResponseDTO::new);
     }
 }
